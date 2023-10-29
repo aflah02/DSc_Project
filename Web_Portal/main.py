9f1e2ba0b6924b8d8c8b84e0447b1f78327be045
Aflah <aflah20082@iiitd.ac.in>
2023-10-29 13:01:43 +0530

    GTrend Statistical Analysis Done

--- Web_Portal/main.py
+++ Web_Portal/main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+import pandas as pd
 
 st.set_page_config(
     page_title="Main",
@@ -11,7 +12,10 @@ data_path = 'Data/'
 def main():
     st.title("Analyzing Bitcoin Heist Data")
 
-
+    if 'df' not in st.session_state:
+        df = pd.read_csv(data_path + 'BitcoinHeistDataWithMonthAndDate.csv')
+    else:
+        df = st.session_state.df
     
 if __name__ == "__main__":
     main()
